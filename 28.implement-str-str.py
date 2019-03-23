2b354319a333e299ee4e5c70a7d0807467de2cdb
Terence Then <chenjienan2009@gmail.com>
2019-03-23 00:26:35 -0700

    daily problem-solving

---
+++ 28.implement-str-str.py
@@ -0,0 +1,51 @@
+#
+# @lc app=leetcode id=28 lang=python3
+#
+# [28] Implement strStr()
+#
+# https://leetcode.com/problems/implement-strstr/description/
+#
+# algorithms
+# Easy (31.44%)
+# Total Accepted:    395K
+# Total Submissions: 1.3M
+# Testcase Example:  '"hello"\n"ll"'
+#
+# Implement strStr().
+# 
+# Return the index of the first occurrence of needle in haystack, or -1 if
+# needle is not part of haystack.
+# 
+# Example 1:
+# 
+# 
+# Input: haystack = "hello", needle = "ll"
+# Output: 2
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: haystack = "aaaaa", needle = "bba"
+# Output: -1
+# 
+# 
+# Clarification:
+# 
+# What should we return when needle is an empty string? This is a great
+# question to ask during an interview.
+# 
+# For the purpose of this problem, we will return 0 when needle is an empty
+# string. This is consistent to C's strstr() and Java's indexOf().
+# 
+#
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        len_h = len(haystack)
+        len_n = len(needle)
+
+        for i in range(len_h - len_n + 1):
+            if haystack[i: i+len_n] == needle:
+                return i
+        return -1
+
---
+++ 6.zig-zag-conversion.py
@@ -0,0 +1,77 @@
+#
+# @lc app=leetcode id=6 lang=python3
+#
+# [6] ZigZag Conversion
+#
+# https://leetcode.com/problems/zigzag-conversion/description/
+#
+# algorithms
+# Medium (30.99%)
+# Total Accepted:    296K
+# Total Submissions: 955.1K
+# Testcase Example:  '"PAYPALISHIRING"\n3'
+#
+# The string "PAYPALISHIRING" is written in a zigzag pattern on a given number
+# of rows like this: (you may want to display this pattern in a fixed font for
+# better legibility)
+# 
+# 
+# P   A   H   N
+# A P L S I I G
+# Y   I   R
+# 
+# 
+# And then read line by line: "PAHNAPLSIIGYIR"
+# 
+# Write the code that will take a string and make this conversion given a
+# number of rows:
+# 
+# 
+# string convert(string s, int numRows);
+# 
+# Example 1:
+# 
+# 
+# Input: s = "PAYPALISHIRING", numRows = 3
+# Output: "PAHNAPLSIIGYIR"
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: s = "PAYPALISHIRING", numRows = 4
+# Output: "PINALSIGYAHRPI"
+# Explanation:
+# 
+# P     I    N
+# A   L S  I G
+# Y A   H R
+# P     I
+# 
+#
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows == 1: return s
+        rows = ['' for _ in range(numRows)]
+        
+        step = 1
+        i = 0
+
+        for c in s:
+            rows[i] += c
+
+            if i == 0:
+                step = 1
+            elif i == numRows - 1:
+                step *= -1
+            i += step
+
+        res = ''
+        for r in rows:
+            res += r
+
+        return res
+        
+
+            
+
